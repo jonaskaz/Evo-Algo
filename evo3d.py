b7643a34c48f47ac273ee76251dbeb88ff275bb6
jonas <jonas.kazlauskas@me.com>
2021-12-07 16:35:15 -0500

    Add ability to constrain the starting population

--- evo3d.py
+++ evo3d.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 import random
 import matplotlib.pyplot as plt
 
@@ -70,37 +69,56 @@ class Population:
     @staticmethod
     def constrain(val, min_val, max_val):
         return min(max_val, max(min_val, val))
+    
+    def in_range(self, ind):
+        return ind[0] >= self.xmin and ind[0] <= self.xmax and ind[1] >= self.ymin and ind[1] <= self.ymax
+    
+    def constrain_pop(self):
+        new_pop = []
+        for ind in self.pop:
+            if self.in_range(ind):
+                new_pop.append(ind)
+        self.pop = np.array(new_pop)
 
     def mutate(self, baby, p_mut = 0.5):
         if random.uniform(0, 1) < p_mut:
-            baby[0] = self.constrain(baby[0]*random.uniform(0.5, 1), self.xmin, self.xmax)
-            baby[1] = self.constrain(baby[1]*random.uniform(0.5, 1), self.ymin, self.ymax)
+            baby[0] = baby[0]*random.uniform(-2, 2)#self.constrain(baby[0]*random.uniform(-1, 2), self.xmin, self.xmax)
+            baby[1] = baby[1]*random.uniform(-2, 2)#self.constrain(baby[1]*random.uniform(-1, 2), self.ymin, self.ymax)
         return baby
     
-    def plot_optimal(self):
-        x = np.arange(self.xmin, self.xmax, 0.2)
-        y = np.arange(self.ymin, self.ymax, 0.2)
+    def plot_optimal(self, xmin=0, xmax=0, ymin=0, ymax=0):
+        x = np.arange(xmin, xmax, 0.2)
+        y = np.arange(ymin, ymax, 0.2)
         x, y = np.meshgrid(x, y)
         z = self.fitness(x, y)
         self.ax.plot_wireframe(x, y, z)
 
     def plot_pop(self):
         self.ax.scatter(self.x, self.y, self.scores, marker="x", c="red")
-        #self.ax.set_title()
+        self.ax.set_title("Population after 6 Epoch")
+        self.ax.set_xlabel("X")
+        self.ax.set_ylabel("Y")
+        self.ax.set_zlabel("Fitness")
 
 
 
 if __name__ == "__main__":
-    xy_range = [-3, 4, -2, 4]
+    #xy_range = [-3, 4, -2, 4] 
+    # Use new constrained range
+    xy_range = [-1, 4, -2, 2]
     pop = Population(1000, * xy_range)
     pop.score()
-    epochs = 3
+    epochs = 100
     for i in range(epochs):
-        pop.selection(int(pop.pop_size/1.39))
+        pop.selection(int(pop.pop_size/1.28))
         print(pop.pop_size)
         pop.reproduce()
         print(pop.pop_size)
         pop.score()
-    pop.plot_optimal()
+        pop.constrain_pop()
+    pop.selection(int(pop.pop_size/1.3))
+    pop.score()
+    pop.constrain_pop()
     pop.plot_pop()
+    pop.plot_optimal(-3, 4, -2, 4)
     plt.show()
